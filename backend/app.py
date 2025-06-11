fe08cfa8feb0644252adb44c49647e43b1df757b
Omachonu Prosper <omachonucodes@gmail.com>
2025-06-11 13:33:25 +0100

    Finish up endpoints

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
+from datetime import timedelta
+from flask_jwt_extended import JWTManager
 from endpoints import endpoint
 from config import Config
-from flask_jwt_extended import JWTManager
+
 
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = Config.JWT_SECRET_KEY
-jwt = JWTManager(app)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=5)
 app.register_blueprint(endpoint)
+jwt = JWTManager(app)
 
 
 
--- backend/endpoints.py
+++ backend/endpoints.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from utils.db import DB
 from utils.response import Response
 from utils.misc import get_access_token
@@ -14,7 +15,7 @@ def home():
 
 @endpoint.route('/categories')
 def fetch_categories():
-    categories = list(DB.categories.find({}, {'_id': 0}))
+    categories = DB.fetch_categories()
     if not len(categories):
         return Response.error(
             msg="No categories found on this server! This shouldnt be",
@@ -45,4 +46,47 @@ def connect_wallet():
     return Response.success(
         msg="Wallet connected successfully",
         data=user
+    )
+
+
+@endpoint.route('/leaderboard')
+@jwt_required()
+def fetch_leaderboard_data():
+    user_id = get_jwt_identity()
+    return Response.success(
+        msg='This is a protected route',
+        data=DB.get_leaderboard_data(user_id)
+    )
+
+
+@endpoint.route('/quiz/questions/<string:category_id>')
+def fetch_questions_by_category_id(category_id):
+    if not DB.fetch_category(category_id):
+        return Response.error(
+            msg='Category not found',
+            code=404
+        )
+        
+    questions = DB.fetch_questions_by_category(category_id)    
+    return Response.success(
+        msg=f'Questions fetched for category <{category_id}> fetched successfully',
+        data=questions
+    )
+
+
+@endpoint.route('/quiz/record', methods=['POST'])
+@jwt_required()
+def record_quiz_results():
+    score = request.json.get('score')
+    if not score:
+        return Response.error(msg="'score' is a required field")
+    else:
+        try:
+            score = int(score)
+        except:
+            return Response.error(msg="The value for 'score' should be an integer")
+        
+    DB.record_quiz_result(get_jwt_identity(), score)
+    return Response.success(
+        msg="Quiz results recorded"
     )
